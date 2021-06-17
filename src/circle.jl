782cef74660be7728d29ecf82617dad76e768357
Siddharth Bhatia <sidb60200@gmail.com>
2021-06-17 17:06:39 +0530

    update circle drawing algorithm

--- src/circle.jl
+++ src/circle.jl
@@ -4,7 +4,7 @@ struct Circle{I <: Integer} <: AbstractShape
     radius::I
 end
 
-function draw_octant_reflections!(image::AbstractMatrix, i_center::Integer, j_center::Integer, i::Integer, j::Integer, color)
+@inline function draw_octant_reflections!(image::AbstractMatrix, i_center::Integer, j_center::Integer, i::Integer, j::Integer, color)
     image[i_center + i, j_center + j] = color
     image[i_center - i, j_center + j] = color
     image[i_center + i, j_center - j] = color
@@ -18,27 +18,29 @@ function draw_octant_reflections!(image::AbstractMatrix, i_center::Integer, j_ce
 end
 
 """
-Draw a circle. Ref: https://www.geeksforgeeks.org/bresenhams-circle-drawing-algorithm/
+Draw a circle. Ref: https://en.wikipedia.org/wiki/Midpoint_circle_algorithm variant with integer-based arithmetic
 """
 function draw!(image::AbstractMatrix, shape::Circle{I}, color) where {I}
     i_center = shape.i_center
     j_center = shape.j_center
     radius = shape.radius
 
-    i = zero(I)
+    zero_value = zero(I)
+
+    i = zero_value
     j = radius
-    d = 3 - 2 * radius
 
     draw_octant_reflections!(image, i_center, j_center, i, j, color)
 
+    constant = 3 - 2 * radius * radius
+
     while j >= i
+        d = 2 * j * j + 2 * i * i + 4 * i - 2 * j + constant
+
         i += 1
 
-        if d > zero(d)
+        if d > zero_value
             j -= 1
-            d = d + 4 * (i - j) + 10
-        else
-            d = d + 4 * i + 6
         end
 
         draw_octant_reflections!(image, i_center, j_center, i, j, color)
