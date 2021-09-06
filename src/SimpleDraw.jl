e859879e44db327bfeae4f5d95bd6f2fabe0b771
Siddharth Bhatia <sidb60200@gmail.com>
2021-09-06 22:06:08 +0530

    add put_pixel! method

--- src/SimpleDraw.jl
+++ src/SimpleDraw.jl
@@ -2,6 +2,14 @@ module SimpleDraw
 
 abstract type AbstractDrawable end
 
+@inline function put_pixel!(image, i, j, color)
+    if checkbounds(Bool, image, i, j)
+        @inbounds image[i, j] = color
+    end
+
+    return nothing
+end
+
 include("background.jl")
 include("line.jl")
 include("circle.jl")
