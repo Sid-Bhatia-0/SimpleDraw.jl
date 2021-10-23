f1bb97fb24a6724ba636cffe23a1040aa8bbe51a
Siddharth Bhatia <sidb60200@gmail.com>
2021-10-23 10:34:14 +0530

    add draw_inbounds! for Point

--- src/point.jl
+++ src/point.jl
@@ -7,3 +7,8 @@ function draw!(image::AbstractMatrix, shape::Point, color)
     put_pixel!(image, shape.i, shape.j, color)
     return nothing
 end
+
+@inline function draw_inbounds!(image::AbstractMatrix, shape::Point, color)
+    put_pixel_inbounds!(image, shape.i, shape.j, color)
+    return nothing
+end
