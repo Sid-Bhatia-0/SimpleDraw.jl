4cbab2e177b0448dca1da2742edcbee272f8bb2b
Siddharth Bhatia <sidb60200@gmail.com>
2021-10-23 10:45:24 +0530

    replace fill! with image[:, :] .= color for Background

--- src/background.jl
+++ src/background.jl
@@ -3,6 +3,6 @@ struct Background <: AbstractShape end
 @inline draw!(image::AbstractMatrix, shape::Background, color) = draw_inbounds!(image, shape, color)
 
 @inline function draw_inbounds!(image::AbstractMatrix, shape::Background, color)
-    fill!(image, color)
+    @inbounds image[:, :] .= color
     return nothing
 end
