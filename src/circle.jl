7de7c9650c42db63cef4c554e8fdfb88eeb4a128
Siddharth Bhatia <sidb60200@gmail.com>
2021-10-23 15:29:26 +0530

    add draw_inbounds! for FilledCircle

--- src/circle.jl
+++ src/circle.jl
@@ -23,6 +23,15 @@ end
     return nothing
 end
 
+@inline function draw_vertical_strip_reflections_inbounds!(image::AbstractMatrix, i_center::Integer, j_center::Integer, i::Integer, j::Integer, color)
+    draw_inbounds!(image, VerticalLine(i_center - i, i_center + i, j_center - j), color)
+    draw_inbounds!(image, VerticalLine(i_center - j, i_center + j, j_center - i), color)
+    draw_inbounds!(image, VerticalLine(i_center - j, i_center + j, j_center + i), color)
+    draw_inbounds!(image, VerticalLine(i_center - i, i_center + i, j_center + j), color)
+
+    return nothing
+end
+
 @inline function draw_octant_reflections!(image::AbstractMatrix, i_center::Integer, j_center::Integer, i::Integer, j::Integer, color)
     put_pixel!(image, i_center - i, j_center - j, color)
     put_pixel!(image, i_center + i, j_center - j, color)
@@ -133,6 +142,11 @@ function draw!(image::AbstractMatrix, shape::FilledCircle{I}, color) where {I}
     j_center = shape.center.j
     radius = shape.radius
 
+    if checkbounds(Bool, image, i_center - radius, j_center - radius) && checkbounds(Bool, image, i_center + radius, j_center + radius)
+        draw_inbounds!(image, shape, color)
+        return nothing
+    end
+
     zero_value = zero(I)
 
     i = zero_value
@@ -152,6 +166,35 @@ function draw!(image::AbstractMatrix, shape::FilledCircle{I}, color) where {I}
         end
 
         draw_vertical_strip_reflections!(image, i_center, j_center, i, j, color)
+    end
+
+    return nothing
+end
+
+function draw_inbounds!(image::AbstractMatrix, shape::FilledCircle{I}, color) where {I}
+    i_center = shape.center.i
+    j_center = shape.center.j
+    radius = shape.radius
+
+    zero_value = zero(I)
+
+    i = zero_value
+    j = radius
+
+    draw_vertical_strip_reflections_inbounds!(image, i_center, j_center, i, j, color)
+
+    constant = 3 - 2 * radius * radius
+
+    while j >= i
+        d = 2 * j * j + 2 * i * i + 4 * i - 2 * j + constant
+
+        i += 1
+
+        if d > zero_value
+            j -= 1
+        end
+
+        draw_vertical_strip_reflections_inbounds!(image, i_center, j_center, i, j, color)
     end
 
     return nothing
