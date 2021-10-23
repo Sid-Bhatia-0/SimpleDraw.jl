7af60a3376ff964ede4f48f5ec9d7355f000cb7d
Siddharth Bhatia <sidb60200@gmail.com>
2021-10-23 10:43:43 +0530

    add draw_inbounds! for Background

--- src/background.jl
+++ src/background.jl
@@ -1,6 +1,8 @@
 struct Background <: AbstractShape end
 
-@inline function draw!(image::AbstractMatrix, shape::Background, color)
+@inline draw!(image::AbstractMatrix, shape::Background, color) = draw_inbounds!(image, shape, color)
+
+@inline function draw_inbounds!(image::AbstractMatrix, shape::Background, color)
     fill!(image, color)
     return nothing
 end
