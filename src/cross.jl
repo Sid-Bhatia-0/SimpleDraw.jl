ff37a26a24a1aa2547fac48e01183db55fd7778f
Siddharth Bhatia <sidb60200@gmail.com>
2021-10-24 14:39:15 +0530

    use VerticalLine & HorizontalLine in draw! for Cross

--- src/cross.jl
+++ src/cross.jl
@@ -9,19 +9,13 @@ mutable struct HollowCross{I <: Integer} <: AbstractShape
 end
 
 function draw!(image::AbstractMatrix, shape::Cross, color)
-    height, width = size(image)
-
-    i_center = shape.center.i
-    j_center = shape.center.j
+    center = shape.center
+    i_center = center.i
+    j_center = center.j
     radius = shape.radius
 
-    for j in j_center - radius : j_center + radius
-        put_pixel!(image, i_center, j, color)
-    end
-
-    for i in i_center - radius : i_center + radius
-        put_pixel!(image, i, j_center, color)
-    end
+    draw!(image, HorizontalLine(i_center, j_center - radius, j_center + radius), color)
+    draw!(image, VerticalLine(i_center - radius, i_center + radius, j_center), color)
 
     return nothing
 end
