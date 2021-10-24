66fa27cff10ee622d2b28c7f8a2030e854933760
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-10-24 15:11:27 +0530

    add draw_inbounds! for HollowCross (#54)
    * utilize VerticalLine & HorizontalLine in draw! for HollowCross
    
    * add draw_inbounds! for HollowCross

--- src/cross.jl
+++ src/cross.jl
@@ -44,27 +44,46 @@ function draw_inbounds!(image::AbstractMatrix, shape::Cross, color)
 end
 
 function draw!(image::AbstractMatrix, shape::HollowCross, color)
-    height, width = size(image)
-
-    i_center = shape.center.i
-    j_center = shape.center.j
+    center = shape.center
+    i_center = center.i
+    j_center = center.j
     radius = shape.radius
 
-    for j in j_center - radius : j_center + radius
-        if j == j_center
-            continue
-        else
-            put_pixel!(image, i_center, j, color)
-        end
+    i_start = i_center - radius
+    i_end = i_center + radius
+
+    j_start = j_center - radius
+    j_end = j_center + radius
+
+    if checkbounds(Bool, image, i_start, j_start) && checkbounds(Bool, image, i_end, j_end)
+        draw_inbounds!(image, shape, color)
+        return nothing
     end
 
-    for i in i_center - radius : i_center + radius
-        if i == i_center
-            continue
-        else
-            put_pixel!(image, i, j_center, color)
-        end
-    end
+    draw!(image, HorizontalLine(i_center, j_start, j_center - 1), color)
+    draw!(image, VerticalLine(i_start, i_center - 1, j_center), color)
+    draw!(image, VerticalLine(i_center + 1, i_end, j_center), color)
+    draw!(image, HorizontalLine(i_center, j_center + 1, j_end), color)
+
+    return nothing
+end
+
+function draw_inbounds!(image::AbstractMatrix, shape::HollowCross, color)
+    center = shape.center
+    i_center = center.i
+    j_center = center.j
+    radius = shape.radius
+
+    i_start = i_center - radius
+    i_end = i_center + radius
+
+    j_start = j_center - radius
+    j_end = j_center + radius
+
+    draw_inbounds!(image, HorizontalLine(i_center, j_start, j_center - 1), color)
+    draw_inbounds!(image, VerticalLine(i_start, i_center - 1, j_center), color)
+    draw_inbounds!(image, VerticalLine(i_center + 1, i_end, j_center), color)
+    draw_inbounds!(image, HorizontalLine(i_center, j_center + 1, j_end), color)
 
     return nothing
 end
