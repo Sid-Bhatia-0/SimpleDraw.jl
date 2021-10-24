720a412e8b5b82edc4ac901e32e563ad1e496793
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-10-24 15:43:24 +0530

    add draw_inbounds! for PolyLine (#55)

--- src/poly_line.jl
+++ src/poly_line.jl
@@ -19,3 +19,21 @@ function draw!(image::AbstractMatrix, shape::PolyLine, color)
         return nothing
     end
 end
+
+function draw_inbounds!(image::AbstractMatrix, shape::PolyLine, color)
+    points = shape.points
+    num_points = length(points)
+
+    if num_points == 0
+        return nothing
+    elseif num_points == 1
+        draw_inbounds!(image, first(points), color)
+        return nothing
+    else
+        for i in 1 : num_points - 1
+            draw_inbounds!(image, Line(points[i], points[i + 1]), color)
+        end
+
+        return nothing
+    end
+end
