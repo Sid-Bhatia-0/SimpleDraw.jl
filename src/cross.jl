29dd0657e4951c01d645cf9ffaedc87ceb2ef641
Siddharth Bhatia <sidb60200@gmail.com>
2021-10-24 14:42:15 +0530

    add draw_inbounds! for Cross

--- src/cross.jl
+++ src/cross.jl
@@ -14,8 +14,31 @@ function draw!(image::AbstractMatrix, shape::Cross, color)
     j_center = center.j
     radius = shape.radius
 
-    draw!(image, HorizontalLine(i_center, j_center - radius, j_center + radius), color)
-    draw!(image, VerticalLine(i_center - radius, i_center + radius, j_center), color)
+    i_start = i_center - radius
+    i_end = i_center + radius
+
+    j_start = j_center - radius
+    j_end = j_center + radius
+
+    if checkbounds(Bool, image, i_start, j_start) && checkbounds(Bool, image, i_end, j_end)
+        draw_inbounds!(image, shape, color)
+        return nothing
+    end
+
+    draw!(image, HorizontalLine(i_center, j_start, j_end), color)
+    draw!(image, VerticalLine(i_start, i_end, j_center), color)
+
+    return nothing
+end
+
+function draw_inbounds!(image::AbstractMatrix, shape::Cross, color)
+    center = shape.center
+    i_center = center.i
+    j_center = center.j
+    radius = shape.radius
+
+    draw_inbounds!(image, HorizontalLine(i_center, j_center - radius, j_center + radius), color)
+    draw_inbounds!(image, VerticalLine(i_center - radius, i_center + radius, j_center), color)
 
     return nothing
 end
