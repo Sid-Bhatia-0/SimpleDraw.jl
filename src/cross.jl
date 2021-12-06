d0c4e01b4440a458a79e2aaa8b121eea53656a3b
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-12-06 20:57:47 +0530

    refactor cross (#78)
    * refactor Cross
    
    * increase min diameter in is_valid to 3
    
    * refactor HollowCross

--- src/cross.jl
+++ src/cross.jl
@@ -8,6 +8,34 @@ struct HollowCross{I <: Integer} <: AbstractShape
     diameter::I
 end
 
+#####
+##### Cross
+#####
+
+is_valid(shape::Union{Cross, HollowCross}) = shape.diameter > convert(typeof(shape.diameter), 3)
+
+function is_outbounds(shape::Union{Cross, HollowCross}, image::AbstractMatrix)
+    position = shape.position
+    diameter = shape.diameter
+
+    one_value = one(shape.diameter)
+
+    i_min = position.i
+    j_min = position.j
+
+    diameter_minus_1 = diameter - one_value
+    i_max = i_min + diameter_minus_1
+    j_max = j_min + diameter_minus_1
+
+    i_min_image = firstindex(image, 1)
+    i_max_image = lastindex(image, 1)
+
+    j_min_image = firstindex(image, 2)
+    j_max_image = lastindex(image, 2)
+
+    return i_max < i_min_image || i_min > i_max_image || j_max < j_min_image || j_min > j_max_image
+end
+
 function draw!(image::AbstractMatrix, shape::Cross{I}, color) where {I}
     position = shape.position
     i_position = position.i
@@ -17,7 +45,16 @@ function draw!(image::AbstractMatrix, shape::Cross{I}, color) where {I}
     zero_value = zero(I)
     one_value = one(I)
 
-    if diameter < one_value
+    if !is_valid(shape)
+        return nothing
+    end
+
+    if is_outbounds(shape, image)
+        return nothing
+    end
+
+    if diameter == one_value
+        draw!(image, position, color)
         return nothing
     end
 
@@ -27,26 +64,6 @@ function draw!(image::AbstractMatrix, shape::Cross{I}, color) where {I}
     diameter_minus_1 = diameter - one_value
     i_max = i_position + diameter_minus_1
     j_max = j_position + diameter_minus_1
-
-    i_min_image = firstindex(image, 1)
-    i_max_image = lastindex(image, 1)
-
-    j_min_image = firstindex(image, 2)
-    j_max_image = lastindex(image, 2)
-
-    if i_max < i_min_image || i_min > i_max_image || j_max < j_min_image || j_min > j_max_image
-        return nothing
-    end
-
-    if diameter == one_value
-        draw!(image, position, color)
-        return nothing
-    end
-
-    if i_min >= i_min_image && j_min >= j_min_image && i_max <= i_max_image && j_max <= j_max_image
-        draw_unchecked!(image, shape, color)
-        return nothing
-    end
 
     radius = diameter ÷ 2
     i_center = i_position + radius
@@ -58,11 +75,12 @@ function draw!(image::AbstractMatrix, shape::Cross{I}, color) where {I}
     return nothing
 end
 
-function draw_unchecked!(image::AbstractMatrix, shape::Cross, color)
+function _draw!(image::AbstractMatrix, shape::Cross, color)
     position = shape.position
     i_position = position.i
     j_position = position.j
     diameter = shape.diameter
+
     radius = diameter ÷ 2
     i_center = i_position + radius
     j_center = j_position + radius
@@ -73,6 +91,12 @@ function draw_unchecked!(image::AbstractMatrix, shape::Cross, color)
     return nothing
 end
 
+get_bounding_box(shape::Union{Cross, HollowCross}) = Rectangle(shape.position, shape.diameter, shape.diameter)
+
+#####
+##### HollowCross
+#####
+
 function draw!(image::AbstractMatrix, shape::HollowCross{I}, color) where {I}
     position = shape.position
     i_position = position.i
@@ -82,7 +106,11 @@ function draw!(image::AbstractMatrix, shape::HollowCross{I}, color) where {I}
     zero_value = zero(I)
     one_value = one(I)
 
-    if diameter < one_value
+    if !is_valid(shape)
+        return nothing
+    end
+
+    if is_outbounds(shape, image)
         return nothing
     end
 
@@ -90,27 +118,12 @@ function draw!(image::AbstractMatrix, shape::HollowCross{I}, color) where {I}
     j_min = j_position
 
     diameter_minus_1 = diameter - one_value
-    i_max = i_position + diameter_minus_1
-    j_max = j_position + diameter_minus_1
-
-    i_min_image = firstindex(image, 1)
-    i_max_image = lastindex(image, 1)
-
-    j_min_image = firstindex(image, 2)
-    j_max_image = lastindex(image, 2)
-
-    if i_max < i_min_image || i_min > i_max_image || j_max < j_min_image || j_min > j_max_image
-        return nothing
-    end
-
-    if i_min >= i_min_image && j_min >= j_min_image && i_max <= i_max_image && j_max <= j_max_image
-        draw_unchecked!(image, shape, color)
-        return nothing
-    end
+    i_max = i_min + diameter_minus_1
+    j_max = j_min + diameter_minus_1
 
     radius = diameter ÷ 2
-    i_center = i_position + radius
-    j_center = j_position + radius
+    i_center = i_min + radius
+    j_center = j_min + radius
 
     draw!(image, HorizontalLine(i_center, j_min, j_center - one_value), color)
     draw!(image, VerticalLine(i_min, i_center - one_value, j_center), color)
@@ -120,11 +133,12 @@ function draw!(image::AbstractMatrix, shape::HollowCross{I}, color) where {I}
     return nothing
 end
 
-function draw_unchecked!(image::AbstractMatrix, shape::HollowCross{I}, color) where {I}
+function _draw!(image::AbstractMatrix, shape::HollowCross{I}, color) where {I}
     position = shape.position
     i_position = position.i
     j_position = position.j
     diameter = shape.diameter
+
     radius = diameter ÷ 2
     i_center = i_position + radius
     j_center = j_position + radius
@@ -145,7 +159,3 @@ function draw_unchecked!(image::AbstractMatrix, shape::HollowCross{I}, color) wh
 
     return nothing
 end
-
-get_bounding_box(shape::Cross) = Rectangle(shape.position, shape.diameter, shape.diameter)
-
-get_bounding_box(shape::HollowCross) = get_bounding_box(Cross(shape.position, shape.diameter))
