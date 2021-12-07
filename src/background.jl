31390b13e7eac517075b5ced4b2ecbb83f7b7da4
Siddharth Bhatia <sidb60200@gmail.com>
2021-12-07 20:17:38 +0530

    use fill! in draw! for Background

--- src/background.jl
+++ src/background.jl
@@ -3,6 +3,6 @@ struct Background <: AbstractShape end
 @inline draw!(image::AbstractMatrix, shape::Background, color) = _draw!(image, shape, color)
 
 @inline function _draw!(image::AbstractMatrix, shape::Background, color)
-    @inbounds image[:, :] .= color
+    fill!(image, color)
     return nothing
 end
