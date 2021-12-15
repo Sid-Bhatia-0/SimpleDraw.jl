e43b87d7671964ad894038b0afc1030bdc36cdf3
Siddharth Bhatia <sidb60200@gmail.com>
2021-12-16 00:08:00 +0530

    refactor and cleanup line.jl

--- src/line.jl
+++ src/line.jl
@@ -147,34 +147,56 @@ get_bounding_box(shape::HorizontalLine) = Rectangle(Point(shape.i, shape.j_min),
 ##### Line
 #####
 
+function is_inbounds(shape::Line, image::AbstractMatrix)
+    point1 = shape.point1
+    point2 = shape.point2
+
+    i1 = point1.i
+    j1 = point1.j
+    i2 = point2.i
+    j2 = point2.j
+
+    i_min, i_max = minmax(i1, i2)
+    j_min, j_max = minmax(j1, j2)
+
+    i_min_image = firstindex(image, 1)
+    i_max_image = lastindex(image, 1)
+
+    j_min_image = firstindex(image, 2)
+    j_max_image = lastindex(image, 2)
+
+    return i_min >= i_min_image && j_min >= j_min_image && i_max <= i_max_image && j_max <= j_max_image
+end
+
 """
 Draw a line. Ref: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 """
 function draw!(image::AbstractMatrix, shape::Line, color)
-    i1 = shape.point1.i
-    j1 = shape.point1.j
-    i2 = shape.point2.i
-    j2 = shape.point2.j
+    point1 = shape.point1
+    point2 = shape.point2
+
+    i1 = point1.i
+    j1 = point1.j
+    i2 = point2.i
+    j2 = point2.j
 
     if j1 == j2
-        i1, i2 = minmax(i1, i2)
-        draw!(image, VerticalLine(i1, i2, j1), color)
+        i_min, i_max = minmax(i1, i2)
+        draw!(image, VerticalLine(i_min, i_max, j1), color)
         return nothing
     end
 
     if i1 == i2
-        j1, j2 = minmax(j1, j2)
-        draw!(image, HorizontalLine(i1, j1, j2), color)
+        j_min, j_max = minmax(j1, j2)
+        draw!(image, HorizontalLine(i1, j_min, j_max), color)
         return nothing
     end
 
-    if checkbounds(Bool, image, i1, j1) && checkbounds(Bool, image, i2, j2)
-        f = put_pixel_unchecked!
+    if is_inbounds(shape, image)
+        _draw!(put_pixel_unchecked!, image, shape, color)
     else
-        f = put_pixel!
+        _draw!(put_pixel!, image, shape, color)
     end
-
-    _draw!(f, image, shape, color)
 
     return nothing
 end
@@ -182,18 +204,20 @@ end
 _draw!(image::AbstractMatrix, shape::Line, color) = _draw!(put_pixel_unchecked!, image, shape, color)
 
 function _draw!(f::Function, image::AbstractMatrix, shape::Line, color)
-    i1 = shape.point1.i
-    j1 = shape.point1.j
-    i2 = shape.point2.i
-    j2 = shape.point2.j
+    point1 = shape.point1
+    point2 = shape.point2
+
+    i1 = point1.i
+    j1 = point1.j
+    i2 = point2.i
+    j2 = point2.j
 
     I = typeof(i1)
-    one_value = one(I)
 
     di = abs(i2 - i1)
     dj = -abs(j2 - j1)
-    si = i1 < i2 ? one_value : -one_value
-    sj = j1 < j2 ? one_value : -one_value
+    si = i1 < i2 ? one(I) : -one(I)
+    sj = j1 < j2 ? one(I) : -one(I)
     err = di + dj
 
     while true
@@ -222,6 +246,7 @@ end
 function get_bounding_box(shape::Line)
     point1 = shape.point1
     point2 = shape.point2
+
     i1 = point1.i
     j1 = point1.j
     i2 = point2.i
@@ -254,28 +279,61 @@ end
 
 is_valid(shape::ThickLine) = shape.diameter > zero(shape.diameter)
 
+function is_inbounds(shape::ThickLine, image::AbstractMatrix)
+    bounding_box = get_bounding_box(shape)
+    i_min = bounding_box.position.i
+    j_min = bounding_box.position.j
+
+    I = typeof(i_min)
+
+    i_max = i_min + bounding_box.height - one(I)
+    j_max = j_min + bounding_box.width - one(I)
+
+    i_min_image = firstindex(image, 1)
+    i_max_image = lastindex(image, 1)
+
+    j_min_image = firstindex(image, 2)
+    j_max_image = lastindex(image, 2)
+
+    return i_min >= i_min_image && j_min >= j_min_image && i_max <= i_max_image && j_max <= j_max_image
+end
+
 function draw!(image::AbstractMatrix, shape::ThickLine, color)
     @assert is_valid(shape) "Cannot draw invalid shape $(shape)"
 
     point1 = shape.point1
     point2 = shape.point2
     diameter = shape.diameter
-    i1 = point1.i
-    j1 = point1.j
-    i2 = point2.i
-    j2 = point2.j
-    radius = diameter ÷ 2
 
-    I = typeof(i1)
-    one_value = one(I)
+    I = typeof(diameter)
 
-    if checkbounds(Bool, image, i1 - radius, j1 - radius) && checkbounds(Bool, image, i1 + radius - one_value, j1 + radius - one_value) && checkbounds(Bool, image, i2 - radius, j2 - radius) && checkbounds(Bool, image, i2 + radius - one_value, j2 + radius - one_value)
-        f = (image, i, j, color) -> _draw!(image, FilledCircle(Point(i - radius, j - radius), diameter), color)
+    radius = diameter ÷ convert(I, 2)
+
+    if is_inbounds(shape, image)
+        _draw!(image, Line(point1, point2), color) do image, i, j, color
+            _draw!(image, FilledCircle(Point(i - radius, j - radius), diameter), color)
+        end
     else
-        f = (image, i, j, color) -> draw!(image, FilledCircle(Point(i - radius, j - radius), diameter), color)
+        _draw!(image, Line(point1, point2), color) do image, i, j, color
+            draw!(image, FilledCircle(Point(i - radius, j - radius), diameter), color)
+        end
     end
 
-    _draw!(f, image, Line(point1, point2), color)
+    return nothing
+end
+
+function _draw!(image::AbstractMatrix, shape::ThickLine, color)
+    point1 = shape.point1
+    point2 = shape.point2
+    diameter = shape.diameter
+
+    I = typeof(diameter)
+
+    radius = diameter ÷ convert(I, 2)
+
+    _draw!(image, Line(point1, point2), color) do image, i, j, color
+        _draw!(image, FilledCircle(Point(i - radius, j - radius), diameter), color)
+    end
 
     return nothing
 end
@@ -292,8 +350,6 @@ function get_bounding_box(shape::ThickLine)
 
     I = typeof(i1)
     radius = diameter ÷ convert(I, 2)
-
-    one_value = one(I)
 
     if i1 < i2
         i_min = i1 - radius
