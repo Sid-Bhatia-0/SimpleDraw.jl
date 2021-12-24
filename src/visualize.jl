4f0730600cd0a3d4157414ca32d9b672b3f0cea7
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-12-24 17:03:08 +0530

    add axis numbers during visualization (#107)

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,6 +1,25 @@
+function get_axis_label(i::Integer)
+    if 0 <= i <= 9
+        return "0$(i)"
+    elseif 10 <= i <= 99
+        return "$(i)"
+    else
+        return "  "
+    end
+end
+
 function visualize(io::IO, image::AbstractMatrix{Bool})
     height, width = size(image)
+
+    print(io, "  ")
+    for j in 1:width
+        print(io, get_axis_label(j))
+    end
+    println(io)
+
     for i in 1:height
+        print(io, get_axis_label(i))
+
         for j in 1:width
             if image[i, j]
                 print(io, "██")
