bd2df5e1065398ed8f1a8e1204fb5402f9701c98
Siddharth Bhatia <sidb60200@gmail.com>
2021-12-24 16:53:15 +0530

    add get_i_min, get_i_max, get_j_min, get_j_max for Point

--- src/point.jl
+++ src/point.jl
@@ -9,3 +9,9 @@ function draw!(f::Function, image::AbstractMatrix, shape::Point, color)
     f(image, shape.i, shape.j, color)
     return nothing
 end
+
+get_i_min(shape::Point) = shape.i
+get_i_max(shape::Point) = shape.i
+
+get_j_min(shape::Point) = shape.j
+get_j_max(shape::Point) = shape.j
