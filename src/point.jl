aee02ca6042cd612d2c2f797bf2301ddccf7d05b
Siddharth Bhatia <sidb60200@gmail.com>
2021-12-25 00:00:53 +0530

    rearrange point.jl

--- src/point.jl
+++ src/point.jl
@@ -3,15 +3,15 @@ struct Point{I <: Integer} <: AbstractShape
     j::I
 end
 
+get_i_min(shape::Point) = shape.i
+get_i_max(shape::Point) = shape.i
+
+get_j_min(shape::Point) = shape.j
+get_j_max(shape::Point) = shape.j
+
 get_drawing_optimization_style(::Point) = PUT_PIXEL
 
 function draw!(f::Function, image::AbstractMatrix, shape::Point, color)
     f(image, shape.i, shape.j, color)
     return nothing
 end
-
-get_i_min(shape::Point) = shape.i
-get_i_max(shape::Point) = shape.i
-
-get_j_min(shape::Point) = shape.j
-get_j_max(shape::Point) = shape.j
