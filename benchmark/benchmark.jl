0e34ae4328d2573d1dee0ab1e33d677a0cca7027
Siddharth Bhatia <sidb60200@gmail.com>
2022-01-04 16:55:12 +0530

    update benchmark.jl

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -2,35 +2,43 @@ import BenchmarkTools as BT
 import Dates
 import SimpleDraw as SD
 
-const SHAPES = [
-                SD.Point,
-                SD.Background,
-                SD.Line,
-                SD.VerticalLine,
-                SD.HorizontalLine,
-                SD.ThickLine,
-                SD.Circle,
-                SD.ThickCircle,
-                SD.FilledCircle,
-                SD.Rectangle,
-                SD.ThickRectangle,
-                SD.FilledRectangle,
-               ]
+const SHAPE_TYPES = [
+                     SD.Point,
+                     SD.Background,
+                     SD.Line,
+                     SD.ThickLine,
+                     SD.Circle,
+                     SD.FilledCircle,
+                     SD.ThickCircle,
+                     SD.Rectangle,
+                     SD.FilledRectangle,
+                     SD.ThickRectangle,
+                     SD.Character,
+                     SD.TextLine,
+                    ]
 
 const SIZES = [64, 256, 1024]
 
-get_shape(::Type{SD.Point}, n) = SD.Point(n ÷ 2, n ÷ 2)
+get_shape(::Type{SD.Point}, n) = SD.Point(n ÷ 2 + 1, n ÷ 2 + 1)
 get_shape(::Type{SD.Background}, n) = SD.Background()
-get_shape(::Type{SD.VerticalLine}, n) = SD.VerticalLine(2, n - 1, n ÷ 2)
-get_shape(::Type{SD.HorizontalLine}, n) = SD.HorizontalLine(n ÷ 2, 2, n - 1)
-get_shape(::Type{SD.Line}, n) = SD.Line(SD.Point(3, 2), SD.Point(n - 2, n - 1))
-get_shape(::Type{SD.ThickLine}, n) = SD.ThickLine(SD.Point(n ÷ 8, n ÷ 8), SD.Point(n - n ÷ 8 + 1, n - n ÷ 8 + 1), n ÷ 8)
+get_shape(::Type{SD.Line}, n) = SD.Line(SD.Point(n ÷ 8 + 1, 2), SD.Point((7 * n) ÷ 8, n - 1))
+get_shape(::Type{SD.ThickLine}, n) = SD.ThickLine(SD.Point(n ÷ 8 + 1, n ÷ 8 + 1), SD.Point((7 * n) ÷ 8, (7 * n) ÷ 8), (n ÷ 8) - 1)
 get_shape(::Type{SD.Circle}, n) = SD.Circle(SD.Point(2, 2), n - 2)
-get_shape(::Type{SD.ThickCircle}, n) = SD.ThickCircle(SD.Point(2, 2), n - 2, n ÷ 4)
 get_shape(::Type{SD.FilledCircle}, n) = SD.FilledCircle(SD.Point(2, 2), n - 2)
+get_shape(::Type{SD.ThickCircle}, n) = SD.ThickCircle(SD.Point(2, 2), n - 2, n ÷ 4)
 get_shape(::Type{SD.Rectangle}, n) = SD.Rectangle(SD.Point(2, 2), n - 1, n - 1)
-get_shape(::Type{SD.ThickRectangle}, n) = SD.ThickRectangle(SD.Point(2, 2), n - 1, n - 1, n ÷ 4)
 get_shape(::Type{SD.FilledRectangle}, n) = SD.FilledRectangle(SD.Point(2, 2), n - 1, n - 1)
+get_shape(::Type{SD.ThickRectangle}, n) = SD.ThickRectangle(SD.Point(2, 2), n - 1, n - 1, n ÷ 4)
+get_shape(::Type{SD.Character}, n) = SD.Character(SD.Point(2, 2), 'A', SD.TERMINUS_32_16)
+
+function get_shape(::Type{SD.TextLine}, n)
+    str = ""
+    for j in 1 : n ÷ 16
+        str = str * Char(rand(65:90))
+    end
+
+    return SD.TextLine(SD.Point(1, 1), str, SD.TERMINUS_32_16)
+end
 
 function get_benchmarks(shape_types, sizes)
     benchmarks = Dict()
@@ -38,11 +46,16 @@ function get_benchmarks(shape_types, sizes)
     color = 0x00ffffff
     for n in sizes
         image = zeros(typeof(color), n, n)
-        for Shape in shape_types
-            shape = get_shape(Shape, n)
+        for ShapeType in shape_types
+            shape = get_shape(ShapeType, n)
             benchmark = BT.@benchmark SD.draw!($(Ref(image))[], $(Ref(shape))[], $(Ref(color))[])
-            benchmarks[(nameof(Shape), n)] = get_summary(benchmark)
-            @info "(shape = $(nameof(Shape)), n = $(n)) benchmark complete"
+            memory, median_time = get_summary(benchmark)
+            benchmarks[(nameof(ShapeType), n)] = (memory, median_time)
+            println("############################")
+            println("n = $(n)")
+            println("shape = $(Base.limitrepr(shape))")
+            println("median_time = $(median_time)")
+            println("memory = $(memory)")
         end
         fill!(image, zero(color))
     end
@@ -50,7 +63,7 @@ function get_benchmarks(shape_types, sizes)
     return benchmarks
 end
 
-function get_summary(trial::BT.Trial)
+function get_summary(trial)
     median_trial = BT.median(trial)
     memory = BT.prettymemory(median_trial.memory)
     median_time = BT.prettytime(median_trial.time)
@@ -60,7 +73,7 @@ end
 function get_table(shape_types, sizes, benchmarks)
     table = "| |"
     for n in sizes
-        table = table * "$(n)|"
+        table = table * "n = $(n)|"
     end
     table = table * "\n"
 
@@ -70,8 +83,8 @@ function get_table(shape_types, sizes, benchmarks)
     end
     table = table * "\n"
 
-    for Shape in shape_types
-        shape_name = nameof(Shape)
+    for ShapeType in shape_types
+        shape_name = nameof(ShapeType)
         table = table * "|"
         table = table * "$(shape_name)|"
 
@@ -85,7 +98,7 @@ function get_table(shape_types, sizes, benchmarks)
     return table
 end
 
-function generate_benchmark_file(; shape_types = SHAPES, sizes = SIZES, file_name = nothing)
+function generate_benchmark_file(; shape_types = SHAPE_TYPES, sizes = SIZES, file_name = nothing)
     date = Dates.format(Dates.now(), "yyyy_mm_dd_HH_MM_SS")
 
     if isnothing(file_name)
