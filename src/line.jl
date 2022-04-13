ae31e57d9a83eb6ed182c39686d913b5aac6ce4f
Siddharth Bhatia <sidb60200@gmail.com>
2022-04-13 17:45:54 +0530

    convert integer literal to be same type as other operand

--- src/line.jl
+++ src/line.jl
@@ -143,7 +143,7 @@ end
 
 is_valid(shape::ThickLine) = shape.thickness > zero(shape.thickness)
 
-get_i_min(shape::ThickLine) = min(shape.point1.i, shape.point2.i) - shape.thickness ÷ 2
+get_i_min(shape::ThickLine) = min(shape.point1.i, shape.point2.i) - shape.thickness ÷ convert(typeof(shape.thickness), 2)
 
 function get_i_max(shape::ThickLine)
     point1 = shape.point1
@@ -156,12 +156,12 @@ function get_i_max(shape::ThickLine)
 
     i_max = max(point1.j, point2.j)
 
-    half_thickness = thickness ÷ 2
+    half_thickness = thickness ÷ convert(typeof(thickness), 2)
 
     return i_max - half_thickness + thickness - one(I)
 end
 
-get_j_min(shape::ThickLine) = min(shape.point1.j, shape.point2.j) - shape.thickness ÷ 2
+get_j_min(shape::ThickLine) = min(shape.point1.j, shape.point2.j) - shape.thickness ÷ convert(typeof(shape.thickness), 2)
 
 function get_j_max(shape::ThickLine)
     point1 = shape.point1
@@ -172,7 +172,7 @@ function get_j_max(shape::ThickLine)
 
     j_max = max(point1.j, point2.j)
 
-    half_thickness = thickness ÷ 2
+    half_thickness = thickness ÷ convert(typeof(thickness), 2)
 
     return j_max - half_thickness + thickness - one(I)
 end
@@ -193,7 +193,7 @@ function draw!(f::F, image::AbstractMatrix, shape::ThickLine, color) where {F <:
     i2 = point2.i
     j2 = point2.j
 
-    half_thickness = thickness ÷ 2
+    half_thickness = thickness ÷ convert(typeof(thickness), 2)
 
     draw!(image, Line(point1, point2), color) do image, i, j, color
         draw!(f, image, FilledRectangle(Point(i - half_thickness, j - half_thickness), thickness, thickness), color)
