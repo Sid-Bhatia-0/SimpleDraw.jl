3e157386c12b315bd58a8bfdfcc0348b5552c670
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-04-23 13:31:57 +0530

    clarify API in README and export API names (#139)
    * export API types and functions
    
    * clarify API in README

--- src/SimpleDraw.jl
+++ src/SimpleDraw.jl
@@ -13,4 +13,60 @@ include("character.jl")
 include("text.jl")
 include("visualize.jl")
 
+const SHAPE_TYPES = [
+Point,
+Background,
+Line,
+ThickLine,
+Circle,
+FilledCircle,
+ThickCircle,
+Rectangle,
+FilledRectangle,
+ThickRectangle,
+FilledTriangle,
+Character,
+TextLine,
+]
+
+# export abstract shape types
+export AbstractShape
+
+# export concrete shape types
+export Point
+export Background
+export Line
+export ThickLine
+export Circle
+export FilledCircle
+export ThickCircle
+export Rectangle
+export FilledRectangle
+export ThickRectangle
+export FilledTriangle
+export Character
+export TextLine
+
+# export shape validation methods
+export is_valid
+
+# export shape position related methods
+export get_i_min
+export get_i_max
+export get_j_min
+export get_j_max
+export get_i_extrema
+export get_j_extrema
+export get_height
+export get_width
+export get_position
+
+# export shape translation methods
+export move_i
+export move_j
+export move
+
+# shape drawing methods
+export draw!
+
 end
