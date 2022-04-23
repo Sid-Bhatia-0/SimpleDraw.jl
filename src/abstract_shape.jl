9be6ef7c007f384238a8b51f5ee32deed43aae01
Siddharth Bhatia <sidb60200@gmail.com>
2022-04-23 16:27:48 +0530

    add docstring for is_valid

--- src/abstract_shape.jl
+++ src/abstract_shape.jl
@@ -4,6 +4,20 @@
 
 abstract type AbstractShape end
 
+"""
+    is_valid(shape)
+
+Return `true` if `shape` is a valid shape that can be drawn and `false` otherwise.
+
+# Examples
+```julia-repl
+julia> is_valid(Rectangle(Point(9, 5), 16, 24))
+true
+
+julia> is_valid(Rectangle(Point(9, 5), -16, 24))
+false
+```
+"""
 is_valid(::AbstractShape) = true
 
 #####
