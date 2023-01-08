881c44d3e06222bd0beaf5b7689d4c9e1befe27a
Siddharth Bhatia <sidb60200@gmail.com>
2023-01-08 16:59:36 -0600

    add ConsistentFilledTriangle and change some previous FixedTriangle methods to work for AbstractTriangle

--- src/triangle.jl
+++ src/triangle.jl
@@ -1,5 +1,11 @@
 abstract type AbstractTriangle <: AbstractShape end
 
+struct ConsistentFilledTriangle{I <: Integer} <: AbstractTriangle
+    point1::Point{I}
+    point2::Point{I}
+    point3::Point{I}
+end
+
 struct VerticalBaseFilledTriangle{I <: Integer} <: AbstractTriangle
     vertical_line::VerticalLine{I}
     point::Point{I}
@@ -161,19 +167,19 @@ function _draw!(f::F, image, shape::VerticalBaseFilledTriangle, color) where {F 
 end
 
 #####
-##### FilledTriangle
+##### AbstractTriangle
 #####
 
-get_i_min(shape::FilledTriangle) = min(shape.point1.i, shape.point2.i, shape.point3.i)
-get_i_max(shape::FilledTriangle) = max(shape.point1.i, shape.point2.i, shape.point3.i)
+get_i_min(shape::AbstractTriangle) = min(shape.point1.i, shape.point2.i, shape.point3.i)
+get_i_max(shape::AbstractTriangle) = max(shape.point1.i, shape.point2.i, shape.point3.i)
 
-get_j_min(shape::FilledTriangle) = min(shape.point1.j, shape.point2.j, shape.point3.j)
-get_j_max(shape::FilledTriangle) = max(shape.point1.j, shape.point2.j, shape.point3.j)
+get_j_min(shape::AbstractTriangle) = min(shape.point1.j, shape.point2.j, shape.point3.j)
+get_j_max(shape::AbstractTriangle) = max(shape.point1.j, shape.point2.j, shape.point3.j)
 
-move_i(shape::FilledTriangle, i) = FilledTriangle(move_i(shape.point1, i), move_i(shape.point2, i), move_i(shape.point3, i))
-move_j(shape::FilledTriangle, j) = FilledTriangle(move_j(shape.point1, j), move_j(shape.point2, j), move_j(shape.point3, j))
+move_i(shape::AbstractTriangle, i) = typeof(shape)(move_i(shape.point1, i), move_i(shape.point2, i), move_i(shape.point3, i))
+move_j(shape::AbstractTriangle, j) = typeof(shape)(move_j(shape.point1, j), move_j(shape.point2, j), move_j(shape.point3, j))
 
-get_drawing_optimization_style(::FilledTriangle) = CHECK_BOUNDS
+get_drawing_optimization_style(::AbstractTriangle) = CHECK_BOUNDS
 
 function sort_horizontal(point1, point2)
     if point1.j <= point2.j
@@ -201,6 +207,154 @@ function vertical_line_intersection(point1, point2, j)
 
     return i
 end
+
+#####
+##### ConsistentFilledTriangle
+#####
+
+function sort_triangle_points(point1, point2, point3)
+    point1, point2 = sort_points_horizontal_vertical(point1, point2)
+    point1, point3 = sort_points_horizontal_vertical(point1, point3)
+    point2, point3 = sort_points_horizontal_vertical(point2, point3)
+
+    return point1, point2, point3
+end
+
+function _draw!(f::F, image, shape::ConsistentFilledTriangle, color) where {F <: Function}
+    @assert is_valid(shape) "Cannot draw invalid shape $(shape)"
+
+    point1, point2, point3 = sort_triangle_points(shape.point1, shape.point2, shape.point3)
+
+    i1 = point1.i
+    j1 = point1.j
+    i2 = point2.i
+    j2 = point2.j
+    i3 = point3.i
+    j3 = point3.j
+
+    I = typeof(i1)
+
+    di13 = abs(i3 - i1)
+    dj13 = -abs(j3 - j1)
+    si13 = i1 < i3 ? one(I) : -one(I)
+    sj13 = j1 < j3 ? one(I) : -one(I)
+    err13 = di13 + dj13
+    i13 = i1
+    should_increment_j13 = false
+
+    di12 = abs(i2 - i1)
+    dj12 = -abs(j2 - j1)
+    si12 = i1 < i2 ? one(I) : -one(I)
+    sj12 = j1 < j2 ? one(I) : -one(I)
+    err12 = di12 + dj12
+    i12 = i1
+    should_increment_j12 = false
+
+    di23 = abs(i3 - i2)
+    dj23 = -abs(j3 - j2)
+    si23 = i2 < i3 ? one(I) : -one(I)
+    sj23 = j2 < j3 ? one(I) : -one(I)
+    err23 = di23 + dj23
+    i23 = i2
+    should_increment_j23 = false
+
+    j = j1
+
+    while true
+        if !should_increment_j12 && !should_increment_j13
+            _draw!(f, image, VerticalLine(minmax(i12, i13)..., j), color)
+        end
+
+        if (i12 == i2 && j == j2)
+            break
+        end
+
+        if !should_increment_j12
+            e12 = convert(I, 2) * err12
+
+            if (e12 >= dj12)
+                err12 += dj12
+                i12 += si12
+            end
+
+            if (e12 <= di12)
+                should_increment_j12 = true
+            end
+        end
+
+        if !should_increment_j13
+            e13 = convert(I, 2) * err13
+
+            if (e13 >= dj13)
+                err13 += dj13
+                i13 += si13
+            end
+
+            if (e13 <= di13)
+                should_increment_j13 = true
+            end
+        end
+
+        if should_increment_j12 && should_increment_j13
+            err12 += di12
+            err13 += di13
+            j += sj13
+            should_increment_j12 = false
+            should_increment_j13 = false
+        end
+
+    end
+
+    while true
+        if !should_increment_j23 && !should_increment_j13
+            _draw!(f, image, VerticalLine(minmax(i23, i13)..., j), color)
+        end
+
+        if (i23 == i3 && j == j3)
+            break
+        end
+
+        if !should_increment_j23
+            e23 = convert(I, 2) * err23
+
+            if (e23 >= dj23)
+                err23 += dj23
+                i23 += si23
+            end
+
+            if (e23 <= di23)
+                should_increment_j23 = true
+            end
+        end
+
+        if !should_increment_j13
+            e13 = convert(I, 2) * err13
+
+            if (e13 >= dj13)
+                err13 += dj13
+                i13 += si13
+            end
+
+            if (e13 <= di13)
+                should_increment_j13 = true
+            end
+        end
+
+        if should_increment_j23 && should_increment_j13
+            err23 += di23
+            err13 += di13
+            j += sj13
+            should_increment_j23 = false
+            should_increment_j13 = false
+        end
+    end
+
+    return nothing
+end
+
+#####
+##### FilledTriangle
+#####
 
 function _draw!(f::F, image, shape::FilledTriangle, color) where {F <: Function}
     @assert is_valid(shape) "Cannot draw invalid shape $(shape)"
