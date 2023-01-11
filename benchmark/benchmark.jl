da0aae41435a13b286110fb7df6542df2daa0e36
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2023-01-10 20:39:12 -0600

    add Triangle, Bitmap, Image to  benchmark.jl and update it's Manifest.toml (#169)
    * update Manifest.toml to benchmark my local SimpleDraw module
    
    * add Triangle to benchmark.jl
    
    * update order of shapes in benchmark.jl
    
    * add Bitmap to benchmark.jl
    
    * add Image to benchmark.jl
    
    * pin SimpleDraw version to a latest commit for benchmarking

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -3,34 +3,65 @@ import Dates
 import SimpleDraw as SD
 
 const SHAPE_TYPES = [
-                     SD.Point,
                      SD.Background,
+                     SD.Point,
                      SD.Line,
                      SD.ThickLine,
-                     SD.Circle,
-                     SD.FilledCircle,
-                     SD.ThickCircle,
+                     SD.Triangle,
+                     SD.FilledTriangle,
                      SD.Rectangle,
                      SD.FilledRectangle,
                      SD.ThickRectangle,
-                     SD.FilledTriangle,
+                     SD.Circle,
+                     SD.FilledCircle,
+                     SD.ThickCircle,
+                     SD.Bitmap,
+                     SD.Image,
                      SD.Character,
                      SD.TextLine,
                     ]
 
 const SIZES = [64, 256, 1024]
 
-get_shape(::Type{SD.Point}, n) = SD.Point(n ÷ 2 + 1, n ÷ 2 + 1)
 get_shape(::Type{SD.Background}, n) = SD.Background()
+get_shape(::Type{SD.Point}, n) = SD.Point(n ÷ 2 + 1, n ÷ 2 + 1)
 get_shape(::Type{SD.Line}, n) = SD.Line(SD.Point(n ÷ 8 + 1, 2), SD.Point((7 * n) ÷ 8, n - 1))
 get_shape(::Type{SD.ThickLine}, n) = SD.ThickLine(SD.Point(n ÷ 8 + 1, n ÷ 8 + 1), SD.Point((7 * n) ÷ 8, (7 * n) ÷ 8), (n ÷ 8) - 1)
+get_shape(::Type{SD.Triangle}, n) = SD.Triangle(SD.Point(2, 2), SD.Point(n - 1, n ÷ 2), SD.Point(n ÷ 2, n -1))
+get_shape(::Type{SD.FilledTriangle}, n) = SD.FilledTriangle(SD.Point(2, 2), SD.Point(n - 1, n ÷ 2), SD.Point(n ÷ 2, n -1))
+get_shape(::Type{SD.Rectangle}, n) = SD.Rectangle(SD.Point(2, 2), n - 1, n - 1)
+get_shape(::Type{SD.FilledRectangle}, n) = SD.FilledRectangle(SD.Point(2, 2), n - 1, n - 1)
+get_shape(::Type{SD.ThickRectangle}, n) = SD.ThickRectangle(SD.Point(2, 2), n - 1, n - 1, n ÷ 4)
 get_shape(::Type{SD.Circle}, n) = SD.Circle(SD.Point(2, 2), n - 2)
 get_shape(::Type{SD.FilledCircle}, n) = SD.FilledCircle(SD.Point(2, 2), n - 2)
 get_shape(::Type{SD.ThickCircle}, n) = SD.ThickCircle(SD.Point(2, 2), n - 2, n ÷ 4)
-get_shape(::Type{SD.Rectangle}, n) = SD.Rectangle(SD.Point(2, 2), n - 1, n - 1)
-get_shape(::Type{SD.FilledRectangle}, n) = SD.FilledRectangle(SD.Point(2, 2), n - 1, n - 1)
-get_shape(::Type{SD.FilledTriangle}, n) = SD.FilledTriangle(SD.Point(2, 2), SD.Point(n - 1, n ÷ 2), SD.Point(n ÷ 2, n -1))
-get_shape(::Type{SD.ThickRectangle}, n) = SD.ThickRectangle(SD.Point(2, 2), n - 1, n - 1, n ÷ 4)
+
+function get_shape(::Type{SD.Bitmap}, n)
+    position = SD.Point(2, 2)
+    bitmap = falses(n - 2, n - 2)
+    for j in 1:size(bitmap, 2)
+        for i in 1:size(bitmap, 1)
+            bitmap[i, j] = iseven(i + j)
+        end
+    end
+
+    return SD.Bitmap(position, bitmap)
+end
+
+function get_shape(::Type{SD.Image}, n, color_type)
+    position = SD.Point(2, 2)
+    image2 = zeros(color_type, n - 2, n - 2)
+    for j in 1:size(image2, 2)
+        for i in 1:size(image2, 1)
+            if iseven(i + j)
+                image2[i, j] = one(color_type)
+            end
+        end
+    end
+
+    return SD.Image(position, image2)
+end
+
 get_shape(::Type{SD.Character}, n) = SD.Character(SD.Point(2, 2), 'A', SD.TERMINUS_32_16)
 
 function get_shape(::Type{SD.TextLine}, n)
@@ -50,7 +81,11 @@ function get_benchmarks(shape_types, sizes)
         image = zeros(typeof(color), n, n)
         for ShapeType in shape_types
             shape_name = nameof(ShapeType)
-            shape = get_shape(ShapeType, n)
+            if ShapeType == SD.Image
+                shape = get_shape(ShapeType, n, typeof(color))
+            else
+                shape = get_shape(ShapeType, n)
+            end
             benchmark = BT.@benchmark SD.draw!($(Ref(image))[], $(Ref(shape))[], $(Ref(color))[])
             memory, median_time = get_summary(benchmark)
             benchmarks[(shape_name, n)] = (shape, memory, median_time)
